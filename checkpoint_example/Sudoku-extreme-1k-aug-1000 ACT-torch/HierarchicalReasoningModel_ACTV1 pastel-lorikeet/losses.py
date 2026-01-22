73f34ca62873975d7eecaea1785f33edea26d026
renrua52 <rzr23@mails.tsinghua.edu.cn>
2026-01-22 23:58:05 +0800

    move checkpoint_example/ to LFS

---
+++ checkpoint_example/Sudoku-extreme-1k-aug-1000 ACT-torch/HierarchicalReasoningModel_ACTV1 pastel-lorikeet/losses.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f27e777cf5dcc7d9d78e57c92e521ab02808f4976e988bf253a79fd597e35257
+size 3804
---
+++ checkpoint_example/Sudoku-extreme-1k-aug-1000-hint ACT-torch/HierarchicalReasoningModel_ACTV1 military-salamander/hrm_act_v1.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b9c5195fc14c6f451569434905cd16849d6c56ff7723f5844604b48065ebfd39
+size 12161

85ff8c1f987598c945164a03721d002a0667d775
renrua52 <rzr23@mails.tsinghua.edu.cn>
2026-01-23 00:18:38 +0800

    Fixed example ckpt.

--- scripts/example_evaluation.sh
+++ scripts/example_evaluation.sh
@@ -1,7 +1,7 @@
 CKPTS=$(python -c "
-prefix='checkpoints/Sudoku-extreme-1k-aug-1000-hint ACT-torch/HierarchicalReasoningModel_ACTV1 military-salamander/step_'
+prefix='checkpoint_example/Sudoku-extreme-1k-aug-1000-hint ACT-torch/HierarchicalReasoningModel_ACTV1 military-salamander/step_'
 print(','.join([f'{prefix}{i*1302}' for i in range(31, 41)]))
 ")
 python batch_inference.py --checkpoints "$CKPTS" --permute 9 --num_batch 10 --batch_size 100
-# This is a snapshot version. For the full result, use the following command instead:
+# This is a snapshot version. For the full result, use the following instead:
 # python batch_inference.py --checkpoints "$CKPTS" --permute 9

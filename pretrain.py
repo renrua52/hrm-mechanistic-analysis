9da4066b7486dca5ad8e34f01db9b51065570bf2
renrua52 <rzr23@mails.tsinghua.edu.cn>
2026-01-24 14:21:34 +0800

    Fixed return mismatch in pretrain.py.

--- pretrain.py
+++ pretrain.py
@@ -220,7 +220,7 @@ def train_batch(config: PretrainConfig, train_state: TrainState, batch: Any, glo
             train_state.carry = train_state.model.initial_carry(batch)  # type: ignore
 
     # Forward
-    train_state.carry, loss, metrics, _, _ = train_state.model(carry=train_state.carry, batch=batch, return_keys=[])
+    train_state.carry, loss, metrics, _, _, _ = train_state.model(carry=train_state.carry, batch=batch, return_keys=[])
 
     ((1 / global_batch_size) * loss).backward()
 
